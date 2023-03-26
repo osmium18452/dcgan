8ad96bcbc3ce83ab85ca47671d52b1f59f6fa2ab
os <1711042219@qq.com>
2023-03-26 15:35:32 +0800

    no comment

--- main.cpp
+++ main.cpp
@@ -8,11 +8,20 @@ using std::cin;
 
 int main() {
 #ifdef _WIN32
-    std::string data_root = R"(C:\Users\17110\Desktop\causal ad\ts.dataset\swat)";
+//    const std::string data_root = R"(C:\Users\17110\Desktop\causal ad\ts.dataset\swat\)";
+    const std::string data_root=R"(E:\Pycharm Projects\causal.dataset\data\swat\)";
 #else
-    std::string data_root=R"(/remote-home/liuwenbo/pycproj/tsdata/data)";
+    const std::string data_root=R"(/remote-home/liuwenbo/pycproj/tsdata/data/)";
 #endif
-    cout << "data root: " << data_root << endl;
+    const std::string train_set_file="swat.att.n0a1.csv";
+    const std::string test_set_file="swat.nor.csv";
+    std::ifstream input_file(data_root+train_set_file);
+    int line_num=0,column_num=0;
+    for (std::string line_buffer; std::getline(input_file, line_buffer); )
+    {
+        std::cout << line_buffer << std::endl;
+        line_num++;
+    }
     torch::Tensor tensor = torch::eye(3);
     cout << tensor << endl;
     return 0;
